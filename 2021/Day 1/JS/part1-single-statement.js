b70b45ee3ebf4bb25722699f58ca86ed85900fbc
Kumirei <linusson.sixten@gmail.com>
2021-12-04 15:06:07 -0500

    shorter single statement solution

--- 2021/Day 1/JS/part1-single-statement.js
+++ 2021/Day 1/JS/part1-single-statement.js
@@ -8,3 +8,7 @@ var c = file // c stands four count
     .map((n, i, arr) => (c = i < 1 ? 1 : n > arr[i - 1] ? c + 1 : c))
     .at(-1)
 console.log(c)
+
+// Based on Gorbit99's solution
+c = file.split('\n').filter((n, i, arr) => i > 0 && arr[i - 1] < n).length + 1
+console.log(c)

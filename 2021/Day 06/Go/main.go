5d7e62a51d6eacb381f3616d66ba6dbf0be1c110
Kumirei <linusson.sixten@gmail.com>
2021-12-30 15:33:34 -0500

    day 6 in Go

---
+++ 2021/Day 06/Go/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"advent-of-code/2021/goutils"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Task struct {
+	fishes []int
+}
+
+func main() {
+	part1(getData("06", false), 80)
+	part1(getData("06", false), 256)
+}
+
+func part1(data Task, steps int) {
+	for i := 0; i < steps; i++ {
+		data.fishes[8] = popFirst(data.fishes)
+		data.fishes[6] += data.fishes[8]
+	}
+	var sum int = 0
+	for _, val := range data.fishes {
+		sum += val
+	}
+	fmt.Println("Fishes after", steps, "iterations:", sum)
+}
+
+func popFirst(arr []int) int {
+	first := arr[0]
+	copy(arr, arr[1:])
+	arr = arr[:len(arr)-1]
+	return first
+}
+
+func getData(day string, sample bool) Task {
+	// Read data
+	dataSource := "data.txt"
+	if sample {
+		dataSource = "test-data.txt"
+	}
+	data, err := goutils.ReadFileToString(fmt.Sprintf("2021/Day %v/%v", day, dataSource))
+	if err != nil {
+		panic(err)
+	}
+
+	// Convert to proper format
+	fishes := strings.Split(data[0], ",")
+	ages := make([]int, 9)
+	for _, num := range fishes {
+		age, _ := strconv.Atoi(num)
+		ages[age]++
+	}
+
+	// Return task
+	return Task{fishes: ages}
+}
